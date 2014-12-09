916c3c1d9b34a6072a149a6f7fc9e6af143415a2
loicbenoit <loicbenoit@users.noreply.github.com>
2014-12-09 10:51:31 -0500

    Update config.php
    Part of the proposed modifications to have the current language extracted from the URI.

--- application/config/config.php
+++ application/config/config.php
@@ -58,7 +58,7 @@ $config['url_suffix'] = '';
  * This determines which set of language files should be used. Make sure there is
  * an available translation if you intend to use something other than english.
 */
-$config['language']	= 'english';
+$config['language']	= 'en';
 
 /*
  *------------------------------------------------------------------------------
